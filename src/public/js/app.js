68d405ba84705912082494017f594e02b2e8de73
silsim <nov.doyoung@gmail.com>
2022-07-06 20:26:58 +0900

    fake database

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,24 @@
-alert('hi')
+const messageList = document.querySelector('ul')
+const messageForm = document.querySelector('form')
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server 😀");
+})
+
+socket.addEventListener("message", (message)=>{
+  console.log("New message: " + message.data);
+})
+
+socket.addEventListener("close", ()=>{
+  console.log("Not Connected from Server 😈");
+})
+
+
+function megSubmit(event) {
+  event.preventDefault()
+  const input = messageForm.querySelector('input');
+  socket.send(input.value)
+  input.value = ""
+}
+messageForm.addEventListener("submit", megSubmit)
--- src/server.js
+++ src/server.js
@@ -8,12 +8,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"))
-app.get("/*", (req, res) => res.redirect("home"))
+//app.get("/*", (req, res) => res.redirect("home"))
 
 const handleListn = () => console.log('Listening on http://localhost:3300');
 // app.listen(3300, handleListem);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const sockets = []
+
+wss.on("connection", (socket) => {
+  sockets.push(socket)
+  console.log("Connected to Brower😀");
+  socket.on("close", () => {console.log("Not Connected from Server 😈")})
+  socket.on("message", (message) => {
+    sockets.forEach( (aSokets) => aSokets.send(message.toString('utf-8')))
+  })
+})
 
 server.listen(3300, handleListn)
